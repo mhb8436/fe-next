fabf3656e923517cae1018cbcd9b394617ecf200
mhb8436 <mhb8436@gmail.com>
2026-07-28 12:13:26 +0900

    staging2

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <div className="w-full items-center">
-            <h3>Next.js 앱서비스 단독 배포 </h3>
+            <h3>Next.js 앱서비스 단독 배포 - 스테이징 </h3>
             <ol>
               <li>API_URL: {apiUrl}</li>
               <li>BLOB_URL: {blobUrl}</li>
